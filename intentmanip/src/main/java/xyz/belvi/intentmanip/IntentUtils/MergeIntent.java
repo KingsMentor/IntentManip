95baa371b6bb0f5f7ed3c388b12092ef47e66d8c
Nosakhare Belvi <nosakharebelvi@gmail.com>
2016-11-01 17:06:11 +0100

    first commit

--- intentmanip/src/main/java/xyz/belvi/intentmanip/IntentUtils/MergeIntent.java
+++ intentmanip/src/main/java/xyz/belvi/intentmanip/IntentUtils/MergeIntent.java
@@ -14,11 +14,12 @@ import xyz.belvi.intentmanip.IntentUtils.Models.ResolveIntent;
  * Created by zone2 on 10/2/16.
  */
 
-public class MergeIntent {
+public class MergeIntent extends ManipUtils {
 
     public static List<ResolveIntent> mergeIntents(Context context, Intent... intents) {
 
         PackageManager packageManager = context.getPackageManager();
+
 
         List<ResolveIntent> resolveIntents = new ArrayList<>();
         for (Intent intent : intents) {
@@ -27,6 +28,26 @@ public class MergeIntent {
                 resolveIntents.add(new ResolveIntent(res, intent));
             }
         }
+        return resolveIntents;
+    }
+
+    public static List<ResolveIntent> distinctMerge(Context context, Intent... intents) {
+
+        PackageManager packageManager = context.getPackageManager();
+
+
+        ArrayList<String> intentsNameLog = new ArrayList<>();
+        List<ResolveIntent> resolveIntents = new ArrayList<>();
+        for (Intent intent : intents) {
+            List<ResolveInfo> listCam = packageManager.queryIntentActivities(intent, 0);
+            for (ResolveInfo res : listCam) {
+                if (!intentsNameLog.contains(getPackageName(res))) {
+                    intentsNameLog.add(getPackageName(res));
+                    resolveIntents.add(new ResolveIntent(res, intent));
+                }
+            }
+        }
+        intentsNameLog.clear();
         return resolveIntents;
     }
 }
